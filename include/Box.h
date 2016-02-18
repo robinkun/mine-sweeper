045949441dfc69740df3fdfabf85908447893319
robinkun <robinkun96@gmail.com>
2016-02-18 15:49:45 +0900

    contentとexteriorを追加

--- include/Box.h
+++ include/Box.h
@@ -1,4 +1,13 @@
 #pragma once
 
 class Box {
+private:
+  int _content; // 中身
+  int _exterior; // 外面
+
+public:
+  int content() const;
+  int exterior() const;
+  bool content(int c);
+  bool exterior(int e);
 };
--- source/Box.cpp
+++ source/Box.cpp
@@ -1,1 +1,22 @@
 #include "Box.h"
+
+int Box::content() const {
+  return _content;
+}
+
+
+int Box::exterior() const {
+  return _exterior;
+}
+
+
+bool Box::content(int c) {
+  _content = c;
+  return true;
+}
+
+
+bool Box::exterior(int e) {
+  _exterior = e;
+  return true;
+}
